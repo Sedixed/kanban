3f58d7e05ef70ad18a012c4c100a40efec57f080
Jordan <83838658+KinderPinGuiin@users.noreply.github.com>
2022-11-23 16:43:57 +0100

    Fixed exception handler

--- src/Service/TwigService.php
+++ src/Service/TwigService.php
@@ -24,15 +24,21 @@ class TwigService
     /**
      * Load the Twig template at the given path and returns it's content.
      *
-     * @param  string $path       The path of the Twig template.
+     * @param  string $path       The path of the Twig template. If set to null,
+     *                            null is returned
      * @param  array  $parameters The template's parameters.
-     * @return string             The template's content.
+     * @return string             The template's content or null if path is 
+     *                            null.
      */
     public function getTemplateContent(
-        string $path, 
+        ?string $path, 
         array $parameters = []
     ): ?string
     {
+        if ($path == null) {
+            return null;
+        }
+
         // Throw an exception if the path is invalid
         $content = $this->twig->render($path, $parameters);
 
