4933038727bd16261198f880f466228ca0278386
Jordan <83838658+KinderPinGuiin@users.noreply.github.com>
2022-12-21 03:06:48 +0100

    updated deploy script

--- deploy/deploy.bash
+++ deploy/deploy.bash
@@ -2,19 +2,19 @@
 
 # Mise à jour de PHP 7 vers PHP 8
 
-apt-get purge php7.* # Répondre oui puis non
-apt-get autoclean
-apt-get autoremove
+apt-get purge php7.* -y # Répondre oui puis non
+apt-get autoclean -y
+apt-get autoremove -y
 wget -qO - https://packages.sury.org/php/apt.gpg | apt-key add -
 echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | tee /etc/apt/sources.list.d/sury-php.list
-apt update
+apt update -y
 apt install php8.1 -y
 apt install php8.1-common php8.1-mysql php8.1-xml php8.1-xmlrpc php8.1-curl php8.1-gd php8.1-imagick php8.1-cli php8.1-dev php8.1-imap php8.1-mbstring php8.1-opcache php8.1-soap php8.1-zip php8.1-intl -y
 systemctl restart apache2
 
 # Installation de composer
 
-apt install curl
+apt install curl -y
 curl -sS https://getcomposer.org/installer -o composer-setup.php
 HASH=`curl -sS https://composer.github.io/installer.sig`
 php -r "if (hash_file('SHA384', 'composer-setup.php') === '$HASH') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
@@ -23,13 +23,11 @@ php composer-setup.php --install-dir=/usr/bin --filename=composer
 # Configuration du projet
 
 cd /var/www/html
-
-# TODO : Changer la variable APP_ENV en : prod
-
-composer install --no-dev --optimize-autoloader
-composer update
+sed -i 's/APP_ENV=dev/APP_ENV=prod/g' .env
+composer install --no-dev --optimize-autoloader --no-interaction
+composer update --no-interaction
 php bin/console cache:clear
-php bin/console d:m:m
+php bin/console --no-interaction d:m:m
 
 # Configuration de Apache
 
